6961b629f8357b9551aeb0006cfabad6a580aa25
Maurizio <mauriziocasciano7@gmail.com>
2021-10-08 17:12:09 +0200

    SSLPing testpingLetsEncryptRootX1

--- src/test/java/SSLPingTest.java
+++ src/test/java/SSLPingTest.java
@@ -1,3 +1,4 @@
+import org.check.ssl.SSLPing;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -6,5 +7,11 @@ public class SSLPingTest {
     @Test
     public void testPingGoogle() {
         Assertions.assertTrue(SSLPing.ping("google.com", 443));
+    }
+
+
+    @Test
+    public void testPingLetsEncryptRootX1(){
+        Assertions.assertTrue(SSLPing.ping("valid-isrgrootx1.letsencrypt.org", 443));
     }
 }
